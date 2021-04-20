742c9dfcbd45a0a4c6ea6ec1f17676db9d1fc026
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-20 17:24:12 +0300

    add variable declarations and function

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+
 func main() {
-	
+	// The Go compiler will infer types for you with :=
+	// := only used when variable is declared the first time, the very first time
+	// variable is initialized. When re-declaring variable value use just =
+
+	// Defining new variable and manually assigning type
+	// var card string = "Ace of Spades"
+
+	card := newCard()
+	fmt.Println(card)
+
+	card = "Five of Diamonds"
+	fmt.Println(card)
+
+}
+
+// The function will return type 'string'
+func newCard() string {
+	return "Queen of Hearts"
 }
